18becb21ff3a11a49fb9ca76aa5dca6879a20eab
Quwam Binuyo <qbinuyo7@gmail.com>
2025-02-26 15:29:27 +0100

    added the functionality to add new users to the existing array

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,59 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+
+    const fakeId = Date.now();
+
+    const newUser = { id: fakeId, name };
+    const updateUsers = [...users, newUser];
+    setUsers(updateUsers);
+
+    setName("");
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={handleChange}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <h2>Users</h2>
+      {users.map((user) => {
+        const { name, id } = user;
+
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
