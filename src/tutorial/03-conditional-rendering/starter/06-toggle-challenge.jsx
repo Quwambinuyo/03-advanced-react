5228c227e4bbfa3d32ba1849e30e9addb1f7952e
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-31 11:52:30 +0100

    enhance toggle challenge with comments for clarity and improved state management

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 const ToggleChallenge = () => {
+  // State to manage the visibility of the alert component
   const [showAlert, setShowAlert] = useState(false);
 
+  // Function to toggle the showAlert state
   const toggleAlert = () => {
+    // If showAlert is true, set it to false; otherwise, set it to true
     if (showAlert) {
       setShowAlert(false);
       return;
@@ -12,6 +15,7 @@ const ToggleChallenge = () => {
     setShowAlert(true);
   };
 
+  // Alert component to be rendered conditionally
   const Alert = () => {
     return <div className="alert alert-danger">hello world</div>;
   };
@@ -21,9 +25,11 @@ const ToggleChallenge = () => {
       <h2>toggle challenge</h2>
 
       <div>
+        {/* Button to trigger the toggleAlert function */}
         <button type="button" className="btn" onClick={toggleAlert}>
           toggle
         </button>
+        {/* Conditionally render the Alert component based on the showAlert state */}
         {showAlert && <Alert />}
       </div>
     </>
